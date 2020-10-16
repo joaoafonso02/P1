f9935e73b55225f8f93e9d430876b64ab7615d58
EduardoFernandesUA <edu.fernandes@ua.pt>
2020-10-16 11:40:57 +0100

    add ex 1.9 1.10 1.11

---
+++ aula1/ex_1_10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex_1_10 {
+  public static void main(String args[]){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("\n\nValor em Celcius: ");
+    double celcius = scanner.nextDouble();
+
+    double fahrenheit = 1.8f*celcius+32;
+
+    System.out.printf("\n%2.1fº Celcius é equivalente a %2.1fº Fahrenheit\n\n",celcius,fahrenheit);
+
+    scanner.close();
+  }
+}
---
+++ aula1/ex_1_12.java
@@ -0,0 +1,5 @@
+public class ex_1_12 {
+  public static void main(String args[]){
+    System.out.print("ex 1 1 12"); 
+  }
+}

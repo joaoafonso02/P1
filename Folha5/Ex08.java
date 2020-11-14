67d2914a493851452da46863d8b5a9418572fad1
Rodrigo Faria <74474900+JRodrigoFaria@users.noreply.github.com>
2020-11-14 23:47:05 +0000

    Update Ex08.java

--- Folha5/Ex08.java
+++ Folha5/Ex08.java
@@ -3,39 +3,44 @@ import java.util.Scanner;
 public class Ex08 {
     public static final Scanner KB = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.print("\n\nIntroduza valor inicial de x: ");
-        double xInicial = KB.nextDouble();
-        System.out.print("Introduza valor final de x: ");
-        double xFinal = KB.nextDouble();
-        
-        if(xInicial>xFinal){
-            System.out.print("\nValores impossiveis de x!");
-            return;
-        }
-        
-        System.out.print("Introduza numero de elementos: ");
-        int nElementos = getIntRange(2, Integer.MAX_VALUE);
-
-        System.out.print("\n------------------------------------\n");
-        System.out.print("|   x   | 5x2+10x+3 | 7x3+3x2+5x+2 |\n");
-        System.out.print("------------------------------------\n");
-        for(double i=xInicial; i<=xFinal ;i+=(xFinal-xInicial)/(nElementos-1)){
-            System.out.printf("| %5.1f | %9.2f | %12.3f |\n", i, poly3(0, 5, 10, 3, i), poly3(7, 3, 5, 2, i));
-            System.out.print("------------------------------------\n");
+    public static void main(String[] args)
+    {
+        System.out.print("Insert a value for x1: ");
+        double x1 = sc.nextDouble();
+        System.out.print("Insert a value for x2: ");
+        double x2 = sc.nextDouble();
+        System.out.print("Insert the quantity of values to check: ");
+        int rep = sc.nextInt();
+        printer(x1, x2, rep);
+    }
+    
+    public static void printer(double x1, double x2, int rep)
+    {
+        if (numCheck(x1,x2))
+        {
+            draw();
+            System.out.printf("\n|   x   | 5x2+10x+3 | 7x3+3x2+5x+2 |\n");
+            for (double x= x1; x<=x2; x+=(x2-x1)/(rep-1))
+            {
+                draw();
+                System.out.printf("\n| %5.1f | %9.2f | %12.3f |\n", x, poly3(x,0,5,10,3), poly3(x,7,3,5,2));
+            }
+            draw();
         }
     }
-
-    public static double poly3(double a, double b, double c, double d, double x){
-        return a*Math.pow(x,3) + b*Math.pow(x,2) + c*x + d;
+    
+    public static boolean numCheck(double x1, double x2)
+    {
+        return (x1<x2);
     }
-
-    public static int getIntRange(int min, int max){
-        int n = KB.nextInt();
-        while( n<min||n>max ){
-            System.out.printf("Número inválido. Introduza inteiro (%d a %d): ",min,max);
-            n = KB.nextInt();
-        }
-        return n;
+    
+    public static double poly3(double x, int a, int b, int c, int d)
+    {
+        return (a*Math.pow(x,3) + b*Math.pow(x,2) + c*x + d);
     }
+    
+    public static void draw()
+    {
+        for (int i=0; i<36; i++) System.out.print("-");
+    }  
 }

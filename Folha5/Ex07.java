83a6aaefd0d6f8639caf5fd517aeb43a984be7e7
Rodrigo Faria <74474900+JRodrigoFaria@users.noreply.github.com>
2020-11-14 23:41:37 +0000

    Update Ex07.java

--- Folha5/Ex07.java
+++ Folha5/Ex07.java
@@ -1,35 +1,21 @@
 import java.util.Scanner;
 
-public class Ex07 {
-    public static final Scanner KB = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        System.out.print("\n\nIntroduza a: ");
-        int a = getIntRange(1, Integer.MAX_VALUE);
-        System.out.print("Introduza b: ");
-        int b = getIntRange(1, Integer.MAX_VALUE);
-
-        System.out.print("\n");
-        System.out.printf("MDC = %d", mdc(a, b));
-        System.out.print("\n\n");
+public class Ex07
+{   
+    public static final Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args)
+    {
+        System.out.print("Insert a number: ");
+        int a = sc.nextInt();
+        System.out.print("Insert a number: ");
+        int b = sc.nextInt();
+        System.out.printf("The MCD is %d", MCD(a, b));
     }
     
-
-    public static int getIntRange(int min, int max){
-        int n = KB.nextInt();
-        while( n<min||n>max ){
-            System.out.printf("Número inválido. Introduza inteiro (%d a %d): ",min,max);
-            n = KB.nextInt();
-        }
-        return n;
-    }
-
-    public static int mdc(int a, int b){
-        while(b!=0){
-            int temp = a;
-            a = b;
-            b = temp%b;
-        }
-        return a;
+    public static int MCD(int a, int b)
+    {
+        if (b == 0) return a;
+        else return MCD(b, a % b);
     }
 }
